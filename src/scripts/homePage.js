166fea577b8535f97419184519934eb11c3a0a17
Arthur Rocha <85761080+ArtCRocha@users.noreply.github.com>
2021-12-05 13:23:26 -0300

    Add files via upload

---
+++ src/scripts/homePage.js
@@ -0,0 +1,62 @@
+/* header - menu*/
+const btnMenu = document.querySelector('#btnMobile');
+
+btnMenu.addEventListener('click', ()=>{
+    var nav = document.querySelector('.navMobile');
+    nav.classList.toggle('active');
+
+});
+
+/* Menu Scroll Desktop */
+
+const menuItems = document.querySelectorAll('#menuDesktop a[href^="#"]');
+
+menuItems.forEach((item) =>{
+    item.addEventListener('click', scrollToId);
+})
+
+function getScrollToId(element){
+    const id = element.getAttribute('href');
+    return document.querySelector(id).offsetTop;
+}
+
+function scrollPosition(to){
+    window.scroll({
+        top: to,
+        behavior: 'smooth'
+    })
+}
+
+function scrollToId(event){
+    event.preventDefault();
+    const to = getScrollToId(event.target) - 60;
+    scrollPosition(to);
+}
+
+/* Menu Scroll Mobile */
+const navMobile = document.querySelector('.navMobile');
+const menuItemsMobile = document.querySelectorAll('#menuMobile a[href^="#"]');
+
+menuItemsMobile.forEach(item =>{
+    item.addEventListener('click', scrollToId);
+})
+
+function getScrollToId(element){
+    const id = element.getAttribute('href');
+    return document.querySelector(id).offsetTop;
+}
+
+
+function scrollToId(event){
+    event.preventDefault();
+    navMobile.classList.toggle('active');
+    const to = getScrollToId(event.target) - 60;
+    scrollPosition(to);
+}
+
+function scrollPosition(to){
+    window.scroll({
+        top: to,
+        behavior: 'smooth'
+    })
+}
