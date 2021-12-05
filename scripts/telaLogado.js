369a3794bf149c089554ef1d230807b61950446c
Arthur Rocha <85761080+ArtCRocha@users.noreply.github.com>
2021-12-05 13:31:31 -0300

    Delete scripts directory

--- scripts/telaLogado.js
+++
@@ -1,6 +0,0 @@
-var openMenuUser = document.querySelector('.myAccount');
-var menuUser = document.querySelector('#menuUser')
-
-openMenuUser.addEventListener('click',()=>{
-    menuUser.classList.toggle('activeMenuUser');
-})
